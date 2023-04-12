24fb158e15ef4ff79473a8fc1eabdf24762a9635
AlbertChoe <albert.choe73@gmail.com>
2023-04-12 22:20:46 +0700

    Update RNG

--- Fungsi/BonusRNG.py
+++ Fungsi/BonusRNG.py
@@ -5,7 +5,7 @@ def LCG(seed:int,multiplier:int,increment:int,modulus:int,n:int)->int:
 
 import time
 
-def RNG(): #yang dipakai untuk generate 3 random number 
+def RNGKumpul(): #yang dipakai untuk generate 3 random number 
     seed = int(time.time()*1000)
     multiplier = 1103515245
     increment = 12345
@@ -17,7 +17,15 @@ def RNG(): #yang dipakai untuk generate 3 random number
         random_num = seed % 6
         result.append(random_num)
     return result[0],result[1],result[2]
-#Harus disesuaikan sesuai dengan output yang mau
-#kalo untuk bangun random number cuma dari 1 sampai 5.harus disesuain sendiri
-#kalo untuk kumpul random number bisa dari 0 sampai 5.harus disesuain sendiri
-
+def RNGBangun(): #yang dipakai untuk generate 3 random number 
+    seed = int(time.time()*1000)
+    multiplier = 1103515245
+    increment = 12345
+    modulus = 2**31
+    n = 3
+    result = []
+    for i in range(n):
+        seed = LCG(seed, multiplier, increment, modulus, 1)
+        random_num = (seed %5)+1
+        result.append(random_num)
+    return result[0],result[1],result[2]
