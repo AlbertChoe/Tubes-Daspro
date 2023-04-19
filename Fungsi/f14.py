ecb66aa81a0dd3f2d368890e35b4a30df47880f8
Yasra Zhafirah <97837528+spacetimist@users.noreply.github.com>
2023-04-19 13:08:19 +0700

    Update f14.py

--- Fungsi/f14.py
+++ Fungsi/f14.py
@@ -1,10 +1,18 @@
 import os
 
-def save(file, folder, fileName):
+def save(file, fileName):
+    folder = input("Masukkan nama folder: ")
+    print("Saving...")
+    
     if 'save' not in os.listdir():
+        print("Membuat folder save...")
+        print(f"Membuat folder save/{folder}...")
         os.mkdir('save')
     if folder not in os.listdir('save\\'):
+        print(f"Membuat folder save/{folder}...")
         os.mkdir(f'save\\{folder}')
+    
     with open(f'save\\{folder}\\{fileName}', 'w') as f:
         for row in file:
             f.write(';'.join(str(cell) for cell in row) + '\n')
+    print(f"Berhasil menyimpan data di folder save/{folder}!")
