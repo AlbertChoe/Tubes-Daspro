0a4271880834026014194436ae0cdc32a8c11dee
Yasra Zhafirah <97837528+spacetimist@users.noreply.github.com>
2023-04-23 23:27:19 +0700

    Update f14.py

--- Fungsi/f14.py
+++ Fungsi/f14.py
@@ -14,28 +14,31 @@ def save(file, fileName):
     
     for x in range(3):
         with open(f'save\\{folder}\\{fileName[x]}', 'w') as f:
-            if file[x] == 'users':
+            if x == 0:
+                f.write("username;password;role\n")
                 for i in range(102):
                     for j in range(3):
                         if j != 2:
-                            f.write(f'{file[i][j]};')
+                            f.write(f'{file[x][i][j]};')
                         else:
-                            f.write(f'{file[i][j]}')
+                            f.write(f'{file[x][i][j]}')
                     f.write('\n')
-            elif file[x] == 'candi':
+            elif x == 1:
+                f.write("id;pembuat;pasir;batu;air\n")
                 for i in range(100):
-                    for j in range(4):
-                        if j != 3:
-                            f.write(f'{file[i][j]};')
+                    for j in range(5):
+                        if j != 4:
+                            f.write(f'{file[x][i][j]};')
                         else:
-                            f.write(f'{file[i][j]}')
+                            f.write(f'{file[x][i][j]}')
                     f.write('\n')
             else:
+                f.write("nama;deskripsi;jumlah\n")
                 for i in range(3):
                     for j in range(3):
                         if j != 2:
-                            f.write(f'{file[i][j]};')
+                            f.write(f'{file[x][i][j]};')
                         else:
-                            f.write(f'{file[i][j]}')
+                            f.write(f'{file[x][i][j]}')
                     f.write('\n')
     print(f"Berhasil menyimpan data di folder save/{folder}!")
