6005d67ac7d3b66b217425501c337fd2d73e4e29
rawlra <130956885+rawlra@users.noreply.github.com>
2023-04-30 01:43:03 +0800

    Update f07.py

--- Fungsi/f07.py
+++ Fungsi/f07.py
@@ -4,9 +4,9 @@ def kumpul(bahan_bangunan :list) -> list:
     print("Jin found:", pasir, "pasir,", batu, "batu,", air, "air")
     for i in range(3):
         if bahan_bangunan[i][0] == "Pasir":
-            bahan_bangunan[i][2] =int(bahan_bangunan[i][2])+ pasir
+            bahan_bangunan[i][2] =int(bahan_bangunan[i][2]) + pasir
         elif bahan_bangunan[i][0] == "Batu":
-            bahan_bangunan[i][2] = int(bahan_bangunan[i][2]) +batu
+            bahan_bangunan[i][2] = int(bahan_bangunan[i][2]) + batu
         elif bahan_bangunan[i][0] == "Air":
-            bahan_bangunan[i][2] = int(bahan_bangunan[i][2]) +air
+            bahan_bangunan[i][2] = int(bahan_bangunan[i][2]) + air
     return bahan_bangunan
