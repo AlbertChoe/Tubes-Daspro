4dd9102f7f83c3f1ff196e6aeb6155e7ff380863
Yasra Zhafirah <97837528+spacetimist@users.noreply.github.com>
2023-04-30 16:45:33 +0700

    Update f14.py

--- Fungsi/f14.py
+++ Fungsi/f14.py
@@ -11,30 +11,29 @@ def save(file :tuple , fileName:tuple) ->None:
     elif folder not in os.listdir('save\\'):
         print(f"Membuat folder save/{folder}...")
         os.mkdir(f'save\\{folder}')
-    else: 
-        for x in range(3):
-            with open(f'save\\{folder}\\{fileName[x]}', 'w') as f:
-                if x == 0:
-                    f.write("username;password;role\n")
-                    for i in range(102):
-                        for j in range(3):
-                            if j != 2:
-                                f.write(f'{file[x][i][j]};')
-                            else:
-                                f.write(f'{file[x][i][j]}')
-                        f.write('\n')
-                elif x == 1:
-                    f.write("id;pembuat;pasir;batu;air\n")
-                    for i in range(100):
-                        for j in range(5):
-                            if j != 4:
-                                f.write(f'{file[x][i][j]};')
-                            else:
-                                f.write(f'{file[x][i][j]}')
-                        f.write('\n')
-                else:
-                    f.write("nama;deskripsi;jumlah\n")
-                    for i in range(3):
+    for x in range(3):
+        with open(f'save\\{folder}\\{fileName[x]}', 'w') as f:
+            if x == 0:
+                f.write("username;password;role\n")
+                for i in range(102):
+                    for j in range(3):
+                        if j != 2:
+                            f.write(f'{file[x][i][j]};')
+                        else:
+                            f.write(f'{file[x][i][j]}')
+                    f.write('\n')
+            elif x == 1:
+                f.write("id;pembuat;pasir;batu;air\n")
+                for i in range(100):
+                    for j in range(5):
+                        if j != 4:
+                            f.write(f'{file[x][i][j]};')
+                        else:
+                            f.write(f'{file[x][i][j]}')
+                    f.write('\n')
+            else:
+                f.write("nama;deskripsi;jumlah\n")
+                for i in range(3):
                         for j in range(3):
                             if j != 2:
                                 f.write(f'{file[x][i][j]};')
